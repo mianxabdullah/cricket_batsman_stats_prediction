f803dc5f39700b688e24a432cfcb29d106863fed
mianxabdullah <mian.abdullah.ayub@gmail.com>
2025-12-08 18:48:34 +0500

    added train freature in model.py

--- src/model.py
+++ src/model.py
@@ -15,5 +15,17 @@ class ScorePredictor:
         df['VenueAvg'] = df.groupby(['Player', 'Venue'])['Runs'].transform(lambda x: x.expanding().mean().shift(1))
         df = df.fillna(0)       
         return df
+    
+    def train(self, df):
+        df_processed = self.prepare_features(df)
+        features = ['RollingAvg_5', 'VenueAvg'] 
+        target = 'Runs'
+        X = df_processed[features]
+        y = df_processed[target]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+        predictions = self.model.predict(X_test)
+        mae = mean_absolute_error(y_test, predictions)
+        return mae
         
 
