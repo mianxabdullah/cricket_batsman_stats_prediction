aeccd32f82bafe949578523a2e6ff35f22bb5b09
mianxabdullah <mian.abdullah.ayub@gmail.com>
2025-12-09 16:16:04 +0500

    added app.py and loaded css init

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(
+    page_title="Cricket Performance Analytics",
+    page_icon="🏏",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+def load_css():
+    with open("assets/custom.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
